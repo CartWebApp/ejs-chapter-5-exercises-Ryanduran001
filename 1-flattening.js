6d61f7d49672b06cbc533e816fe4a24a1262605a
Ryan Duran <112416371+Ryanduran001@users.noreply.github.com>
2024-02-18 17:17:10 -0800

    real (i dont understand anything)

--- 1-flattening.js
+++ 1-flattening.js
@@ -1,5 +1,14 @@
 let arrays = [[1, 2, 3], [4, 5], [6]];
-// Your code here.
+
+function flatten(lists) {
+    let temp = []
+    for(k = 1; k < lists.length; k++) {
+        for(i = 0; i < lists[k].length; i++) {
+            temp.push(lists[k[i]]);
+        }
+    }
+    return lists[0].concat(temp)
+}
 
 
 // → [1, 2, 3, 4, 5, 6]
